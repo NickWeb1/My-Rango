80fcedf3d8c36c949ace993f56c309abb93afb99
NickWeb1 <NichCason@hotmail.com>
2015-08-20 17:47:50 +0300

    Added Category, Page models. Populated them with test script

--- TangoDjango/rango/views.py
+++ TangoDjango/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def index(request):
     context_dict = {'bold_message': 'About'}
     return render(request, 'rango/index.html', context_dict)
